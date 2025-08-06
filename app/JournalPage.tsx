1ca9727ae6bcf26dabf55dd16e24bd2a796382c6
Simon <simoncun2586@gmail.com>
2025-08-06 11:03:06 -0700

    fixed styling issues with background

--- app/JournalPage.tsx
+++ app/JournalPage.tsx
@@ -13,6 +13,7 @@ import {
 } from "react-native";
 import { useRouter } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function JournalPage() {
   const router = useRouter();
@@ -36,110 +37,108 @@ export default function JournalPage() {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    <LinearGradient
+      colors={[global.cutie.gradientStart, global.cutie.gradientEnd]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
       style={{ flex: 1 }}
     >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View className="flex-1">
-          {/* Exit Button */}
-          <Pressable
-            onPress={() => router.back()}
-            className="absolute top-20 left-6 z-10"
-          >
-            <AntDesign name="left" size={30} color={global.cutie.pink} />
-          </Pressable>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View className="flex-1">
+            <Pressable
+              onPress={() => router.back()}
+              className="absolute top-20 left-6 z-10"
+            >
+              <AntDesign name="left" size={30} color={global.cutie.pink} />
+            </Pressable>
 
-          <ScrollView
-            contentContainerStyle={{ flexGrow: 1 }}
-            keyboardShouldPersistTaps="handled"
-          >
-            {/* Top Greeting & Mood Section */}
-            <View className="px-10 pt-40 pb-6">
-              <Text className="text-4xl font-nunito-bold mb-2">Hi, Kelly.</Text>
-              <Text className="text-lg text-gray-600 font-nunito">
-                How are you feeling today?
-              </Text>
+            <ScrollView
+              contentContainerStyle={{ flexGrow: 1 }}
+              keyboardShouldPersistTaps="handled"
+            >
+              <View className="px-10 pt-36 pb-6">
+                <Text className="text-4xl font-nunito-bold mb-2">
+                  Hi, Kelly.
+                </Text>
+                <Text className="text-lg text-gray-600 font-nunito">
+                  How are you feeling today?
+                </Text>
 
-              <View className="flex-row justify-between items-start mt-10 mr-5">
-                <View>
-                  {moods.map((item, index) => (
-                    <Pressable
-                      key={item}
-                      onPress={() => setMood(item)}
-                      className={`rounded-full px-4 py-2 shadow ${
-                        mood === item
-                          ? item === "super awesome"
-                            ? "bg-cutie-pink"
-                            : item === "pretty good"
-                              ? "bg-cutie-orange"
-                              : item === "okay"
-                                ? "bg-cutie-green"
-                                : item === "pretty bad"
-                                  ? "bg-cutie-blue"
-                                  : item === "really terrible"
-                                    ? "bg-cutie-purple"
-                                    : "bg-gray-400"
-                          : "bg-white"
-                      } ${index !== moods.length - 1 ? "mb-3" : ""}`}
-                    >
-                      <Text
-                        className={`${
+                <View className="flex-row justify-between items-start mt-10 mr-5">
+                  <View>
+                    {moods.map((item, index) => (
+                      <Pressable
+                        key={item}
+                        onPress={() => setMood(item)}
+                        className={`rounded-full px-4 py-2 shadow ${
                           mood === item
-                            ? "text-white font-nunito-bold"
-                            : "text-gray-800 font-nunito-bold"
-                        }`}
+                            ? item === "super awesome"
+                              ? "bg-cutie-pink"
+                              : item === "pretty good"
+                                ? "bg-cutie-orange"
+                                : item === "okay"
+                                  ? "bg-cutie-green"
+                                  : item === "pretty bad"
+                                    ? "bg-cutie-blue"
+                                    : item === "really terrible"
+                                      ? "bg-cutie-purple"
+                                      : "bg-gray-400"
+                            : "bg-white"
+                        } ${index !== moods.length - 1 ? "mb-3" : ""}`}
                       >
-                        {item}
-                      </Text>
-                    </Pressable>
-                  ))}
-                </View>
+                        <Text
+                          className={`${
+                            mood === item
+                              ? "text-white font-nunito-bold"
+                              : "text-gray-800 font-nunito-bold"
+                          }`}
+                        >
+                          {item}
+                        </Text>
+                      </Pressable>
+                    ))}
+                  </View>
 
-                <View className="items-center w-1/2">
-                  <Image
-                    source={moodToImage[mood]}
-                    className="w-60 h-60"
-                    resizeMode="contain"
-                  />
+                  <View className="items-center w-1/2">
+                    <Image
+                      source={moodToImage[mood]}
+                      className="w-60 h-60"
+                      resizeMode="contain"
+                    />
+                  </View>
                 </View>
               </View>
-            </View>
-
-            {/* Journal "Bottom Sheet" */}
-            <View className="bg-white rounded-t-3xl shadow border-t border-gray-200 flex-1">
-              {/* Gray line */}
-              <View className="h-1 bg-gray-300 w-16 self-center mt-3 rounded-full" />
-
-              {/* Title */}
-              <Text className="text-gray-700 font-nunito-bold mt-4 mb-2 ml-6 uppercase">
-                My Journal
-              </Text>
-
-              {/* Text Input */}
-              <TextInput
-                placeholder="type here..."
-                placeholderTextColor="#9CA3AF"
-                multiline
-                value={journalText}
-                onChangeText={setJournalText}
-                className="px-6 pt-2 pb-6 text-base text-gray-800 font-nunito"
-                style={{
-                  textAlignVertical: "top",
-                  minHeight: 200,
-                }}
-              />
-
-              {/* Save Button */}
-              <Pressable className="bg-cutie-orange py-3 rounded-full items-center shadow mx-6 translate-y-10">
-                <Text className="text-white font-semibold text-lg font-nunito-bold">
-                  Save
+              <View className="bg-white rounded-t-3xl shadow border-t border-gray-200 flex-1">
+                <View className="h-1 bg-gray-300 w-16 self-center mt-3 rounded-full" />
+                <Text className="text-gray-700 font-nunito-bold mt-4 mb-2 ml-6 uppercase">
+                  My Journal
                 </Text>
-              </Pressable>
-            </View>
-          </ScrollView>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+                <TextInput
+                  placeholder="type here..."
+                  placeholderTextColor="#9CA3AF"
+                  multiline
+                  value={journalText}
+                  onChangeText={setJournalText}
+                  className="px-6 pt-2 pb-6 text-base text-gray-800 font-nunito"
+                  style={{
+                    textAlignVertical: "top",
+                    minHeight: 200,
+                  }}
+                />
+                <Pressable className="bg-cutie-orange py-3 rounded-full items-center shadow mx-6 translate-y-10">
+                  <Text className="text-white font-semibold text-lg font-nunito-bold">
+                    Save
+                  </Text>
+                </Pressable>
+              </View>
+            </ScrollView>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </LinearGradient>
   );
 }
