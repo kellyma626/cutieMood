466d3c1a676bf90e85e9151485b4d264f6d301cb
kellyma073 <kma073@ucr.edu>
2025-08-05 23:14:01 -0700

    add nunito font

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,23 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import {
+  useFonts,
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_700Bold,
+} from "@expo-google-fonts/nunito";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View
       style={{
@@ -9,7 +26,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>cutieMood</Text>
+      <Text style={{ fontFamily: "Nunito_700Bold", fontSize: 24 }}>
+        calendar
+      </Text>
+      <Link
+        href="/new_entry"
+        style={{
+          fontFamily: "Nunito_400Regular",
+          fontSize: 16,
+          marginTop: 10,
+          color: "blue",
+        }}
+      >
+        click to add mood
+      </Link>
     </View>
   );
 }
