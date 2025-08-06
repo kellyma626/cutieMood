14666231acfe0dafc7e57a6bdef3d1aed832d70d
Simon <simoncun2586@gmail.com>
2025-08-06 00:04:54 -0700

    navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,31 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+export default function CustomNavbar() {
+  return (
+    <View className="flex-row items-center justify-between bg-white px-6 py-3 border-t border-black bottom-0">
+      <TouchableOpacity className="flex items-center ml-8">
+        <Image
+          source={require("../assets/images/Today.png")}
+          className="w-16 h-16 mb-1"
+          resizeMode="contain"
+        />
+        <Text className="text-xs text-black">calendar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity className="-mt-24">
+        <Image
+          source={require("../assets/images/Add.png")}
+          className="w-28 h-28"
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity className="flex items-center mr-8">
+        <Image
+          source={require("../assets/images/Chat.png")}
+          className="w-16 h-16 mb-1"
+          resizeMode="contain"
+        />
+        <Text className="text-xs text-black">chat</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,7 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
-import {
-  useFonts,
-  Nunito_400Regular,
-  Nunito_500Medium,
-  Nunito_700Bold,
-} from "@expo-google-fonts/nunito";
 
 export default function Index() {
-  const [fontsLoaded] = useFonts({
-    Nunito_400Regular,
-    Nunito_500Medium,
-    Nunito_700Bold,
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-2xl" style={{ fontFamily: "Nunito_700Bold" }}>
