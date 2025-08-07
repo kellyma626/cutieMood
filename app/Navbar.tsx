28c21ddb4790713de06d4314ee77c7eb1affbf60
Simon <simoncun2586@gmail.com>
2025-08-06 19:56:22 -0700

    more styling stuff and formatting for the chatbot

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -21,10 +21,12 @@ export default function CustomNavbar() {
 
       <Pressable
         className="flex items-center -translate-y-10 bg-white rounded-full"
-        onPress={() => router.push({
-          pathname: "/EntryViewPage",
-          params: { date: "2025-08-06" },
-        })}
+        onPress={() =>
+          router.push({
+            pathname: "/EntryViewPage",
+            params: { date: "2025-08-06" },
+          })
+        }
       >
         <AntDesign name="pluscircle" size={70} color={global.cutie.orange} />
       </Pressable>
